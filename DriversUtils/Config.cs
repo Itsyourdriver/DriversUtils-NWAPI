f3a878352f4a695f89b53dcb5bf303f390620d17
Itsyourdriver <73563507+Itsyourdriver@users.noreply.github.com>
2024-06-30 18:08:09 -0700

    ok

--- DriversUtils/Config.cs
+++ DriversUtils/Config.cs
@@ -18,7 +18,7 @@ namespace Plugin
         public bool ShouldCassie { get; set; } = true;
 
         [Description("Cassie announcement when serpents hand spawn. Only valid cassie words will work")]
-        public string CassieMessage { get; set; } = "Attention All Personnel The Serpents .G5 Hand have been detected inside the facility . All .G2 remaining personnel are to pitch_0.7 jam_056_4 .G1 .G2 .G4 pitch_1.2 NOSCPSLEFT .G6";
+        public string CassieMessage { get; set; } = "Attention All Personnel The Serpents .G5 Hand have entered the facility . All .G2 remaining personnel are to pitch_0.7 jam_056_4 .G1 .G2 .G4 pitch_1.2 NOSCPSLEFT .G6";
 
         [Description("Cassie announcement when flamingos spawn. Only valid cassie words will work. ")]
         public string FlamingoCassieMessage { get; set; } = "Attention All Personnel a new group of SCP 1 5 0 7 .G5 has been detected . All remaining .g6 . are to report to jam_45_3 .g4 .g5 .g2 . pitch_1.2 NOSCPSLEFT .G6";
@@ -35,11 +35,14 @@ namespace Plugin
         [Description("Should Serpent's Hand spawn?")]
         public bool ShouldSerpentsSpawn { get; set; } = true;
 
+        [Description("Should The Science Team's spawn?")]
+        public bool ShouldScienceTeamSpawn { get; set; } = true;
+
         [Description("Should Serpent's Hand spawn more than once?")]
         public bool ShouldSerpentsHandSpawnMore { get; set; } = false;
 
         [Description("Broadcast text to all new serpents hand agents upon spawn")]
-        public string SerpentsHandText { get; set; } = "You have been spawned as a <color=#FF96DE>Serpent's Hand</color> agent. Follow Orders. Work with the SCPs. Terminate other classes.";
+        public string SerpentsHandText { get; set; } = "You have been spawned as a <color=#FF96DE>Serpent's Hand</color> agent. Work with the SCPs. Terminate other classes.";
 
         [Description("Broadcast text to all new serpents hand agents upon spawn")]
         public string SerpentsHandCaptainText { get; set; } = "You have been spawned as a <color=#FF96DE>Serpent's Hand</color> Captain. Command your unit. Work with the SCPs. Terminate other classes.";
@@ -47,11 +50,17 @@ namespace Plugin
         [Description("Should a player be picked as a guard captain upon spawn?")]
         public bool ShouldGuardCaptainsSpawn { get; set; } = true;
 
+        [Description("CAN MTF Nu-7 Spawn in instead of epsillon 11?")]
+        public bool CanNu7Spawn { get; set; } = true;
+
         [Description("Broadcast text to the selected guard captain.")]
-        public string GuardText { get; set; } = "You are the <color=#50C878>Guard Captain.</color> Command Your Unit. Check your inventory.";
+        public string GuardText { get; set; } = "<color=#50C878>Guard Captain.</color> Command Your Unit. Check your inventory.";
 
         [Description("Broadcast text to the selected kid class")]
-        public string KidText { get; set; } = "You are the kid. You can take 3 candy instead of 2.";
+        public string KidText { get; set; } = "You are the kid. You start with candy and are shorter than everyone else!";
+
+        [Description("Text sent to players who find themselves being the last alive.")]
+        public string LastOneAliveHint { get; set; } = "<color=red>You are the last one alive!</color>";
 
         [Description("Should we load item commands? Not implemented yet.")]
         public bool CommandsEnabled { get; set; } = true;
